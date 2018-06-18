927e87001b1a4ae811db8500bb8ce272080ac6ec
anmeng <anmeng@live.cn>
2018-06-18 19:22:25 +0800

    a

--- autoCheckin/src/main/java/com/didispace/dao/WeixinUsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.didispace.dao;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.didispace.entity.WeixinUsers;
-
-public interface WeixinUsersRepository extends JpaRepository<WeixinUsers, Long> {
-	Optional<WeixinUsers> findByOpenid(String openid);
-}
